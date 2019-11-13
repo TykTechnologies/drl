99188c39a08cdda35db22d14febc832435e69240
gernest <geofreyernest@live.com>
2019-11-13 15:11:17 +0300

    initialize stopC channel

--- drl.go
+++ drl.go
@@ -51,6 +51,7 @@ func (d *DRL) CurrentTokenValue() int64 {
 func (d *DRL) Init(ctx context.Context) {
 	d.Servers = NewCache(4 * time.Second)
 	d.RequestTokenValue = 100
+	d.stopC = make(chan struct{})
 	d.serverIndex = make(map[string]Server)
 	d.open.Store(true)
 	go d.startLoop(ctx)
