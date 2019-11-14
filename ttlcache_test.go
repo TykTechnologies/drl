8e919c9d49163a4bbb674db9ca14d555419672f7
gernest <geofreyernest@live.com>
2019-11-14 09:55:42 +0300

    update cache tests

--- ttlcache_test.go
+++ ttlcache_test.go
@@ -1,65 +1,40 @@
 package drl
 
 import (
-	"strconv"
+	"context"
 	"testing"
 	"time"
 )
 
 func TestCache(t *testing.T) {
 	t.Run("IsOpen", func(ts *testing.T) {
-		c := NewCache(0)
+		c := NewCache(context.Background(), 0)
 		if !c.IsOpen() {
 			t.Error("expected the cache to be open")
 		}
 		c.Close()
 	})
 	t.Run("Close", func(ts *testing.T) {
-		c := NewCache(0)
+		c := NewCache(context.Background(), 0)
 		c.Close()
 		if c.IsOpen() {
 			t.Error("expected the cache to be closed")
 		}
 	})
 	t.Run("Item evictions", func(ts *testing.T) {
-		// This test ensures that we are correctly evicting expired items. We set ttl
-		// to me 1ms, this will force the eviction loop to use 1s interval for
-		// eviction.
-		c := NewCache(time.Millisecond)
+		c := NewCache(context.Background(), 0)
 		defer c.Close()
-
-		// Ticking at 100ms we will reach 1s after 10 ticks, meaning any items added
-		// after 10th tick will be available in the cache because tey will be evicted
-		// on 20th tick.
-		tick := time.NewTicker(100 * time.Millisecond)
-		defer tick.Stop()
-		var n int64
-		for range tick.C {
-			n++
-			if n == 15 {
-				break
-			}
-			c.Set(strconv.FormatInt(n, 10), Server{})
-		}
+		c.Set("key", Server{})
+		time.Sleep(2 * time.Second)
 		count := c.Count()
-		if count != 5 {
-			ts.Errorf("expected 5 items to remain in the cache got %d instead", count)
-		}
-		_, ok := c.Get("14")
-		if ok {
-			t.Error("expected the key to have expired")
-		}
-		key := "key"
-		c.Set(key, Server{})
-		_, ok = c.Get(key)
-		if !ok {
-			t.Error("expected the key to exist")
+		if count != 0 {
+			ts.Errorf("expected 0 items to remain in the cache got %d instead", count)
 		}
 	})
 }
 
 func BenchmarkCache_Set(b *testing.B) {
-	c := NewCache(time.Millisecond)
+	c := NewCache(context.Background(), time.Millisecond)
 	s := Server{}
 	key := "1"
 	defer c.Close()
